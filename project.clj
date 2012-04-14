bd0dbb6f63c54dd2140a2507df188fd76dcb25e7
Marshall T. Vandegrift <llasram@gmail.com>
2012-04-14 18:33:48 -0400

    Switch to lein2 project.clj.

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject inet.data "0.1.0-SNAPSHOT"
-  :description "Represent and manipulate Internet networks, domains, etc."
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :source-path "src/clojure"
-  :java-source-path "src/java"
-  :javac-options {:debug "true" :fork "true"}
+  :description "Represent and manipulate various Internet entities as data."
+  :url "http://github.com/llasram/inet.data"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [hier-set "1.1.1"]]
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
+  :javac-options ["-g"]
   :warn-on-reflection true)

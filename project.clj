a10feb6b2b99b1e63dd58464ccfd2b971679c8a6
Marshall T. Vandegrift <llasram@gmail.com>
2012-05-22 11:10:36 -0400

    Use lein-ragel plugin.

--- project.clj
+++ project.clj
@@ -5,8 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [hier-set "1.1.1"]]
+  :plugins [[lein-ragel "0.1.0"]]
   :source-paths ["src/clojure"]
-  :java-source-paths ["src/java"]
+  :java-source-paths ["src/java" "target/ragel"]
+  :ragel-source-paths ["src/ragel"]
   :javac-options ["-g"]
+  :prep-tasks [ragel javac compile]
   :profiles {:dev {:dependencies [[byteable "0.1.1"]]
                    :warn-on-reflection true}})

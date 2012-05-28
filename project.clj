dc4a85c82ac139783f63b1f09f2970b6371bf6b8
Marshall T. Vandegrift <llasram@gmail.com>
2012-05-28 13:18:43 -0400

    Release version 0.1.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject inet.data "0.1.0-SNAPSHOT"
+(defproject inet.data "0.1.0"
   :description "Represent and manipulate various Internet entities as data."
   :url "http://github.com/llasram/inet.data"
   :license {:name "Eclipse Public License"

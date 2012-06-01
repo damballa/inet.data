36c3a6b83c672af097581e3bc7ac46c06f85b3bd
Marshall T. Vandegrift <llasram@damballa.com>
2012-06-01 15:10:35 -0400

    Begin work on next minor version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject inet.data "0.2.0"
+(defproject inet.data "0.3.0-SNAPSHOT"
   :description "Represent and manipulate various Internet entities as data."
   :url "http://github.com/llasram/inet.data"
   :license {:name "Eclipse Public License"

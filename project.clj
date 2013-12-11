3cd0151b5574b10fda68775a46daac21c9d1b815
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2013-12-11 14:03:14 -0500

    Modernize project.clj.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/llasram/inet.data"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [hier-set "1.1.2"]]
   :plugins [[lein-ragel "0.1.0"]
@@ -10,10 +11,11 @@
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java" "target/ragel"]
   :ragel-source-paths ["src/ragel"]
-  :javac-options ["-g"]
+  :javac-options ["-target" "1.6" "-source" "1.6"]
   :prep-tasks ["ragel" "javac"]
-  :warn-on-reflection true
   :codox {:exclude [inet.data.util]}
-  :profiles {:dev {:dependencies [[byteable "0.2.0"]
-                                  [com.damballa/abracad "0.4.0"]
-                                  [criterium "0.4.1"]]}})
+  :profiles {:dev {:dependencies [[criterium "0.4.2"]]}
+             :provided
+             {:dependencies
+              [[byteable "0.2.0"]
+               [com.damballa/abracad "0.4.8"]]}})

79f963e600287bb892253fb38b0f6233bab89638
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2013-12-11 14:36:36 -0500

    Release version 0.5.4.

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Inet.data is available on Clojars.  Add this `:dependency` to your Leiningen
 `project.clj`:
 
 ```clj
-[inet.data "0.5.3"]
+[inet.data "0.5.4"]
 ```
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject inet.data "0.5.4-SNAPSHOT"
+(defproject inet.data "0.5.4"
   :description "Represent and manipulate various Internet entities as data."
   :url "http://github.com/llasram/inet.data"
   :license {:name "Eclipse Public License"

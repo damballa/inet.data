84faa7673325852c5f91384cb73eeae3ecc80e09
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2015-07-21 07:54:33 -0400

    Support JVM >=7 and Clojure >=1.6.

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [hier-set "1.1.2"]]
   :plugins [[lein-ragel "0.1.0"]
             [codox "0.6.6"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java" "target/ragel"]
   :ragel-source-paths ["src/ragel"]
-  :javac-options ["-target" "1.6" "-source" "1.6"]
+  :javac-options ["-target" "1.7" "-source" "1.7"]
   :prep-tasks ["ragel" "javac"]
   :codox {:src-dir-uri "https://github.com/damballa/inet.data/blob/master/"
           :src-linenum-anchor-prefix "L"
@@ -19,15 +19,12 @@
           :exclude [inet.data.util]
           :output-dir "tmp/codox"}
   :aliases {"prep" ["do" "ragel," "javac"]
-            "all" ["with-profile" ~(str "+clojure-1-4:"
-                                        "+clojure-1-5:"
-                                        "+clojure-1-6")]}
+            "all" ["with-profile" ~(str "+clojure-1-6:"
+                                        "+clojure-1-7")]}
   :profiles {:provided {:dependencies
                         [[byteable "0.2.0"]
                          [com.damballa/abracad "0.4.10"]]}
-             :clojure-1-4 {:dependencies
-                           [[org.clojure/clojure "1.4.0"]]}
-             :clojure-1-5 {:dependencies
-                           [[org.clojure/clojure "1.5.1"]]}
              :clojure-1-6 {:dependencies
-                           [[org.clojure/clojure "1.6.0"]]}})
+                           [[org.clojure/clojure "1.6.0"]]}
+             :clojure-1-7 {:dependencies
+                           [[org.clojure/clojure "1.7.0"]]}})

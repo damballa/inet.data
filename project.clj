c5facfcdcd2f41caee768137d51c556acb59d7ce
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2015-07-21 08:41:26 -0400

    Begin development on next iteration.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject inet.data "0.5.6"
+(defproject inet.data "0.5.7-SNAPSHOT"
   :description "Represent and manipulate various Internet entities as data."
   :url "http://github.com/llasram/inet.data"
   :license {:name "Eclipse Public License"
